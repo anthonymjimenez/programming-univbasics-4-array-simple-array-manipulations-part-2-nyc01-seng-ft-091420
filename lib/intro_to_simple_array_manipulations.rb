e148e057f581b2bbb7fe1edac123b03d653b5e4d
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-01 02:32:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+ def using_concat(array, bArray)
+   array.concat(bArray)
+ end
+
+ def using_insert(array, elm)
+   array.insert(4, elm)
+ end
+
+ def using_uniq(array)
+   array.uniq
+ end
+
+ def using_flatten(array)
+   array.flatten
+ end
+
+ def using_delete(array, string)
+   array.delete(string)
+ end
+
+ def using_delete_at(array, int)
+   array.delete_at(int)
+ end
+
+ 
